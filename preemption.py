f65fb552d3d12302c3d1d2de1a010ab7ba203438
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2024-05-13 18:48:56 -0700

    Remove hanging GET

--- preemption.py
+++ preemption.py
@@ -1,11 +1,11 @@
 import requests
 
 def is_preempted_on_gcp():
-    METADATA_URL = 'http://metadata.google.internal/computeMetadata/v1/instance/preempted?wait_for_change=true'
+    METADATA_URL = 'http://metadata.google.internal/computeMetadata/v1/instance/preempted'
     METADATA_HEADERS = {'Metadata-Flavor': 'Google'}
 
     try:
-        response = requests.get(METADATA_URL, headers=METADATA_HEADERS, timeout=None)
+        response = requests.get(METADATA_URL, headers=METADATA_HEADERS)
         if response.status_code == 200:
             return response.text.strip() == 'TRUE'
     except Exception as e:
